ee34ce854cca9358305becf24d6d60b73f8879d9
Jamm3e3333 <Jamm3e3333@gmail.com>
2020-09-14 22:16:57 +0200

    http request

--- files/6_async/app.js
+++ files/6_async/app.js
@@ -1,11 +1,10 @@
 const request = require('request');
 const url = 'http://api.weatherstack.com/current?access_key=ee1f407f492625b5e74f4c638bb1eed0&query=37.8267,-122.4233';
 
-request({url: url}, (error, response) => {
-    // console.log(response);
-    const data = JSON.parse(response.body);
-    // console.log(data);
-    console.log(data.current);
+request({url: url, json: true}, (error, response) => {
+    const location = response.body.location;
+    const weather = response.body.current;
+    console.log(`${weather.weather_descriptions[0]} \nCity: ${location.name} \nCurrent temperature: ${weather.temperature} °C\nFeels like: ${weather.feelslike} °C`);
 })
 
 const APIkey = 'ee1f407f492625b5e74f4c638bb1eed0';
