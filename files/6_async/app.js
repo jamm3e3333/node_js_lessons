4259cec71cecbd8011a94acfebffb66dc7ec99bd
Jamm3e3333 <Jamm3e3333@gmail.com>
2020-09-14 21:35:57 +0200

    weather app starting

--- files/6_async/app.js
+++ files/6_async/app.js
@@ -1,11 +1,11 @@
-console.log('Starting');
+const request = require('request');
+const url = 'http://api.weatherstack.com/current?access_key=ee1f407f492625b5e74f4c638bb1eed0&query=37.8267,-122.4233';
 
-setTimeout(() => {
-    console.log('2 Second timer');
-}, 2000);
+request({url: url}, (error, response) => {
+    // console.log(response);
+    const data = JSON.parse(response.body);
+    // console.log(data);
+    console.log(data.current);
+})
 
-setTimeout(() => {
-    console.log('0 second timer');
-}, 0);
-
-console.log('Stopping');
+const APIkey = 'ee1f407f492625b5e74f4c638bb1eed0';
