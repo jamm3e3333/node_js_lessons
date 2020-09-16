e66f675c3744c90bc1b04e194364a6d1f255d3fa
Jamm3e3333 <Jamm3e3333@gmail.com>
2020-09-16 10:06:19 +0200

    callback review

--- files/7_cb_review/app.js
+++ files/7_cb_review/app.js
@@ -1,8 +1,33 @@
-setTimeout(() => {
-    console.log('Two seconds are up');
-}, 2000);
+// setTimeout(() => {
+//     console.log('Two seconds are up');
+// }, 2000);
 
-const names = ['Andrew','Jen','Jess']
-const shortNames = names.filter((name) => {
-    return name.length <= 4;
+// const names = ['Andrew','Jen','Jess']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4;
+// });
+
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             long: 0
+//         }
+//         callback(data);    
+//     },2000);
+// }
+
+// geocode('Philadelphia', (data) => {
+//     console.log(data);
+// });
+
+const add = (a,b,cb) => {
+    setTimeout(() =>{
+        const sum = a + b;
+        cb(sum);
+    },2000);
+};
+
+add(3,4,(sum) => {
+    console.log(sum);
 })
