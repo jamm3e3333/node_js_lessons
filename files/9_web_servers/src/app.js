7cf06f43f29fb882354ee3fd92d6d2a7db1e4c5c
Jamm3e3333 <Jamm3e3333@gmail.com>
2020-10-13 22:45:40 +0200

    hbs template

--- files/9_web_servers/src/app.js
+++ files/9_web_servers/src/app.js
@@ -2,15 +2,34 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-
 const publicDirectory = path.join(__dirname,'../public');
-
-app.use(express.static(publicDirectory))
 
 app.listen(3000,() => {
     console.log('Server is up on port 3000.');
 });
 
+app.set('view engine','hbs');
+app.use(express.static(publicDirectory))
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Weather app',
+        name: 'Jakub Vala'
+    });
+});
+
+app.get('/about',(req,res) => {
+    res.render('about',{
+        position: 'student',
+        place: 'VSB-TUO'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        read: 'FAQ'
+    })
+})
 
 app.get('/weather', (req,res) => {
     res.send([{
