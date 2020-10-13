1928cc956ab264292e8509db2a1e4ccb70a86295
Jamm3e3333 <Jamm3e3333@gmail.com>
2020-10-13 23:30:33 +0200

    getting hbs with express

--- files/9_web_servers/src/app.js
+++ files/9_web_servers/src/app.js
@@ -2,14 +2,22 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+
+//Define paths for Express config
 const publicDirectory = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname,'../templates')
+
+//setup handlebars engine and views location
+app.set('view engine','hbs');
+app.set('views', viewsPath);
+
+
+//set up the local directory to serve
+app.use(express.static(publicDirectory));
 
 app.listen(3000,() => {
     console.log('Server is up on port 3000.');
 });
-
-app.set('view engine','hbs');
-app.use(express.static(publicDirectory))
 
 app.get('', (req, res) => {
     res.render('index',{
