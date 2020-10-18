7af5d2ae59c0c316992437f9db33d40968f2b1a2
Jamm3e3333 <Jamm3e3333@gmail.com>
2020-10-18 22:49:17 +0200

    styling

--- files/MySQL_and_node/src/app.js
+++ files/MySQL_and_node/src/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const hbs = require('hbs');
+const db = require('./utils/dbSetUp.js');
 
 
 const publicDirectoryPath = path.join(__dirname,'../public');
 const viewsPath = path.join(__dirname,'../templates/views');
 const partialsPath = path.join(__dirname,'../templates/partials');
 const port = process.env.PORT || 3000;
+
+db.conn(db.db);
 
 app.set('view engine','hbs');
 app.set('views',viewsPath);
@@ -19,6 +22,18 @@ app.get('',(req,res) => {
         title: 'Welcome'
     })
 });
+
+app.get('/login',(req, res) => {
+    res.render('login',{
+
+    });
+});
+
+app.get('/register', (req, res) => {
+    res.render('register',{
+
+    })
+})
 
 app.listen(port,() => {
     console.log(`Listening on port: ${port}`);
--- files/MySQL_and_node/src/utils/dbSetUp.js
+++ files/MySQL_and_node/src/utils/dbSetUp.js
@@ -1,9 +1,34 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+    path: path.join(__dirname,'./.env')
+})
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'jack'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_USE,
+    database: process.env.DATABASE
 });
+
+const conn = (database) => {
+    database.connect((error, result) => {
+        if(error){
+            console.log(error);
+        }
+        else{
+            console.log('MySQL connected');
+        }
+    });
+}
+
+
+module.exports = {
+    conn,
+    db
+};
+
 
 
