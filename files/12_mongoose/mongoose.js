2b934e608d1a57085d6a1576c2f86787c24ee7ec
JakubVala <jamm3e3333@gmail.com>
2020-11-24 23:43:26 +0100

    model creating

--- files/12_mongoose/mongoose.js
+++ files/12_mongoose/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true ,
@@ -7,22 +8,81 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be a postive number.');
+            }
+        }
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is not valid!');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value){
+            if(value.length < 6){
+                throw new Error('The password must be longer than 6 characters.');
+            }
+            else if(value.toLowerCase().includes('password')){
+                throw new Error('The password cannot contain a word \"password\".');
+            }
+        }
+    }
+})
+
+const Task = mongoose.model('Task',{
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        required: false,
+        default: false
     }
 })
 
 const me = new User({
     name: 'Jackop',
-    age: 25
-})
+    email: 'JAMM@GOOGLE.COM',
+    password: 'hesoyam'
+});
 
-me.save()
+
+const work = new Task({
+    description: 'Sleeping'
+});
+
+// me.save()
+//     .then(() => {
+//         console.log(me);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+
+
+work.save()
     .then(() => {
-        console.log(me);
+        console.log(work);
     })
-    .catch((error) => {
+    .catch((err) => {
         console.log(error);
     })
